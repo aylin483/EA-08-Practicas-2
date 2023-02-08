af4f38415528e67db5c88e7a1d41350ed2456e39
Usuario <Usuario@DESKTOP-O9E2ATM>
2023-02-08 15:03:24 -0600

    Práctica de sitio web

---
+++ src/main/java/practica_01/PracticaApplication.java
@@ -0,0 +1,13 @@
+package practica_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaApplication.class, args);
+	}
+
+}
